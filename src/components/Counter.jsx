e56215603ed8eb8a496fb1245ee3513c7342530e
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-24 23:50:04 +0545

    simple counter -recap

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment </button>
+      <button onClick={() => setCount(count - 1)}>Decrement </button>
+    </div>
+  );
+};
+
+export default Counter;
