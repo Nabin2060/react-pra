8e0a207ec801210cce604737a941640fbf4384ac
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-10-25 22:24:18 +0545

    header

---
+++ country-search-project/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <span>Country Search</span>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
