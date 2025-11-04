c18bd6155a6e1f98f2719b3539bfc7d85c0447cb
Nabin2060 <141759067+Nabin2060@users.noreply.github.com>
2025-11-04 23:28:02 +0545

    header practice

--- country-search-project/src/components/Header.jsx
+++ country-search-project/src/components/Header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import Searchbar from "./Searchbar";
 
 const Header = () => {
   return (
     <>
       <div>
-        <div>
-          <span>Country Search</span>
+        <div
+          style={{
+            height: "60px",
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "10px",
+            backgroundColor: "#4a5759",
+            color: "white"
+          }}
+        >
+          <span
+            style={{ fontWeight: "bold", fontSize: "24px", marginLeft: "20px" }}
+          >
+            Where is the world?
+          </span>
+          <button
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              fontSize: "16px",
+              marginRight: "20px",
+              backgroundColor: "transparent"
+            }}
+          >
+            Dark Mode
+          </button>
         </div>
-        <div></div>
+        <Searchbar />
       </div>
     </>
   );
